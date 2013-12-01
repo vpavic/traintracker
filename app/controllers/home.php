52ab662668550ca7d69e1e14e183d7ac65d372b9
Vedran Pavic <vedran.pavic@gmail.com>
2013-12-01 18:44:04 +0100

    Implement site preference handling

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -11,13 +11,68 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
-		if ($this->agent->is_mobile())
+		$sitePreference = $this->_get_requested_site_preference();
+
+		if ($sitePreference != false)
 		{
-			redirect('/mobile');
+			$this->_save_site_preference($sitePreference);
 		}
 		else
 		{
+			$sitePreference = $this->_get_saved_site_preference();
+		}
+
+		if ($sitePreference == false)
+		{
+			$sitePreference = $this->_get_default_site_preference();
+		}
+
+		if ($sitePreference == 'desktop')
+		{
 			$this->load->view('home');
+		}
+		elseif ($sitePreference == 'mobile')
+		{
+			redirect('/mobile');
+		}
+	}
+
+	private function _get_requested_site_preference()
+	{
+		$sitePreference = $this->input->get('site_preference');
+		return $this->_validate_site_preference($sitePreference);
+	}
+
+	private function _get_saved_site_preference()
+	{
+		$sitePreference = $this->input->cookie('site_preference', true);
+		return $this->_validate_site_preference($sitePreference);
+	}
+
+	private function _get_default_site_preference()
+	{
+		return $this->agent->is_mobile() ? 'mobile' : 'desktop';
+	}
+
+	private function _save_site_preference($value)
+	{
+		$cookie = array(
+			'name' => 'site_preference',
+			'value' => $value,
+			'expire' => '31536000'
+		);
+
+		$this->input->set_cookie($cookie);
+	}
+
+	private function _validate_site_preference($value)
+	{
+		switch ($value) {
+			case 'desktop':
+			case 'mobile':
+				return $value;
+			default:
+				return false;
 		}
 	}
 }
