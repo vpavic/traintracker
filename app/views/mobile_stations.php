59fd69967a6c1861d44a79bf6d620dd9c9dde1c3
Vedran Pavic <vedran.pavic@gmail.com>
2013-12-02 20:02:25 +0100

    Replace nested lists with collapsible listviews

--- app/views/mobile_stations.php
+++ app/views/mobile_stations.php
@@ -7,7 +7,7 @@
 	<link rel="stylesheet" href="//code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css">
 </head>
 <body>
-	<div data-role="page" class="type-interior">
+	<div data-role="page">
 		<div data-role="header">
 			<h1>Vozni red uživo!</h1>
 			<a href="<?php echo site_url('/mobile'); ?>" data-role="button" data-icon="home" data-iconpos="notext" data-theme="c" data-inline="true" class="ui-btn-right">Početna</a>
@@ -25,23 +25,23 @@
 							<p>Odlazak: <strong><?php echo $current_station['departure_time']; ?></strong> Kašnjenje: <strong><?php echo $current_station['departure_delay']; ?></strong></p>
 						<?php endif; ?>
 					</li>
-					<li>
-						<h3>Potpuni pregled kretanja vlaka</h3>
-						<ul>
-							<?php foreach ($all_stations as $station): ?>
-								<li>
-									<h3><?php echo $station['name']; ?></h3>
-									<?php if ($station['arrival_time'] != ""): ?>
-										<p>Dolazak: <strong><?php echo $station['arrival_time']; ?></strong> Kašnjenje: <strong><?php echo $station['arrival_delay']; ?></strong></p>
-									<?php endif; ?>
-									<?php if ($station['departure_time'] != ""): ?>
-										<p>Odlazak: <strong><?php echo $station['departure_time']; ?></strong> Kašnjenje: <strong><?php echo $station['departure_delay']; ?></strong></p>
-									<?php endif; ?>
-								</li>
-							<?php endforeach; ?>
-						</ul>
-					</li>
 				</ul>
+				<div data-role="collapsible">
+					<h2>Potpuni pregled kretanja vlaka</h2>
+					<ul data-role="listview" data-theme="d" data-divider-theme="d">
+						<?php foreach ($all_stations as $station): ?>
+							<li>
+								<h3><?php echo $station['name']; ?></h3>
+								<?php if ($station['arrival_time'] != ""): ?>
+									<p>Dolazak: <strong><?php echo $station['arrival_time']; ?></strong> Kašnjenje: <strong><?php echo $station['arrival_delay']; ?></strong></p>
+								<?php endif; ?>
+								<?php if ($station['departure_time'] != ""): ?>
+									<p>Odlazak: <strong><?php echo $station['departure_time']; ?></strong> Kašnjenje: <strong><?php echo $station['departure_delay']; ?></strong></p>
+								<?php endif; ?>
+							</li>
+						<?php endforeach; ?>
+					</ul>
+				</div>
 			<?php else: ?>
 				<div class="ui-body ui-body-d ui-corner-all">
 					<p>Podaci o kretanju vlaka br. <?php echo $train_no; ?> trenutno nisu dostupni!</p>
