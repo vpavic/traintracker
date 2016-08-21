c701b17112c85549b5968565812723f7dd71c17d
Vedran Pavic <vedran.pavic@gmail.com>
2016-08-21 15:19:09 +0200

    Simplify main app config

--- src/main/java/io/traintracker/app/TrainTrackerApplication.java
+++ src/main/java/io/traintracker/app/TrainTrackerApplication.java
@@ -1,13 +1,30 @@
 package io.traintracker.app;
 
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class TrainTrackerApplication {
+@EnableCaching
+public class TrainTrackerApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrainTrackerApplication.class, args);
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+	}
+
+	@Bean
+	public CloseableHttpClient httpClient() {
+		return HttpClients.createDefault();
 	}
 
 }
