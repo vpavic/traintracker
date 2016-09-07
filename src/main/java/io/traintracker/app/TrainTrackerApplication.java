b3b767dd596cf29c28a54823c8adfbab82658fbf
Vedran Pavic <vedran.pavic@gmail.com>
2016-09-07 22:50:57 +0200

    Enable i18n support

--- src/main/java/io/traintracker/app/TrainTrackerApplication.java
+++ src/main/java/io/traintracker/app/TrainTrackerApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @SpringBootApplication
 @EnableCaching
@@ -18,6 +21,11 @@ public class TrainTrackerApplication extends WebMvcConfigurerAdapter {
 	}
 
 	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LocaleChangeInterceptor());
+	}
+
+	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("home");
 	}
@@ -25,6 +33,13 @@ public class TrainTrackerApplication extends WebMvcConfigurerAdapter {
 	@Bean
 	public CloseableHttpClient httpClient() {
 		return HttpClients.createDefault();
+	}
+
+	@Bean
+	public CookieLocaleResolver localeResolver() {
+		CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+		localeResolver.setCookieName("LOCALE");
+		return localeResolver;
 	}
 
 }
