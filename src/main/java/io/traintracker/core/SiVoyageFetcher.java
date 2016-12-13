d34b9f80b89c4fcf7a41716ad7d84daf8fbdea29
Vedran Pavic <vedran.pavic@gmail.com>
2016-12-13 19:57:53 +0100

    Add support for Slovenian Railways

--- src/main/java/io/traintracker/core/SiVoyageFetcher.java
+++ src/main/java/io/traintracker/core/SiVoyageFetcher.java
@@ -3,7 +3,10 @@ package io.traintracker.core;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -14,21 +17,20 @@ import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 class SiVoyageFetcher extends AbstractVoyageFetcher {
+
+	private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
 	private static final Charset CHARSET = Charset.forName("Cp1250");
 
-	private static final Pattern PATTERN = Pattern.compile("<tbody>\r\n" +
-			"  <tr>\r\n" +
-			"    <td>(?<name>.*?)</td>\r\n" +
-			"    <td>.*?</td>\r\n" +
-			"    <td>(?<arrivalTime>.*?)</td>\r\n" +
-			"    <td>(?<arrivalDelay>.*?)</td>\r\n" +
-			"    <td>.*?</td>\r\n" +
-			"    <td>(?<departureTime>.*?)</td>\r\n" +
-			"    <td>(?<departureDelay>.*?)</td>\r\n" +
-			"  </tr>");
+	private static final Pattern PATTERN = Pattern.compile("<tr>\r\n" +
+			"                <td class=\"thTitle\">(?<name>.*?)</td>\r\n" +
+			"                <td class=\"tdPrice\">(?<arrivalTime>.*?)</td>\r\n" +
+			"                <td class=\"tdPrice\">(?<departureTime>.*?)</td>\r\n" +
+			"                </tr>");
+
+	private static final ZoneId ZONE_ID = ZoneId.of("Europe/Ljubljana");
 
 	public SiVoyageFetcher(CloseableHttpClient httpClient) {
 		super(httpClient);
@@ -39,10 +41,11 @@ class SiVoyageFetcher extends AbstractVoyageFetcher {
 		URI uri;
 
 		try {
-			uri = new URIBuilder("http://ice.slo-zeleznice.si/CIDirect/default.asp")
-					.addParameter("Category", "E-zeleznice")
-					.addParameter("Service", "w_zamude_web_2_1")
-					.addParameter("vlak", train)
+			uri = new URIBuilder("http://www.slo-zeleznice.si/sl/potniki/vozni-redi/zamude")
+					.addParameter("view", "train")
+					.addParameter("tmpl", "component%")
+					.addParameter("da", LocalDate.now(ZONE_ID).format(FORMATTER))
+					.addParameter("vl", train)
 					.build();
 		}
 		catch (URISyntaxException e) {
@@ -56,24 +59,16 @@ class SiVoyageFetcher extends AbstractVoyageFetcher {
 		while (matcher.find()) {
 			String name = matcher.group("name").trim();
 			String arrivalTime = matcher.group("arrivalTime").trim();
-			String arrivalDelay = matcher.group("arrivalDelay").trim();
 			String departureTime = matcher.group("departureTime").trim();
-			String departureDelay = matcher.group("departureDelay").trim();
 
 			Station station = new Station(name);
 
 			if (!Strings.isNullOrEmpty(arrivalTime) && !arrivalTime.equals(".")) {
 				station.setArrivalTime(LocalTime.parse(arrivalTime));
-				if (!arrivalDelay.equals("R")) {
-					station.setArrivalDelay(Integer.parseInt(arrivalDelay));
-				}
 			}
 
 			if (!Strings.isNullOrEmpty(departureTime) && !departureTime.equals(".")) {
 				station.setDepartureTime(LocalTime.parse(departureTime));
-				if (!departureDelay.equals("R")) {
-					station.setDepartureDelay(Integer.parseInt(departureDelay));
-				}
 			}
 
 			stations.add(station);
