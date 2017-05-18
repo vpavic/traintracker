0c18f9b168e99e8a21a966291970c4b523a6652d
Vedran Pavic <vedran.pavic@gmail.com>
2017-05-18 23:32:17 +0200

    Fix deprecation warnings

--- src/main/java/io/traintracker/config/WebMvcConfiguration.java
+++ src/main/java/io/traintracker/config/WebMvcConfiguration.java
@@ -4,13 +4,13 @@ import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrationsAda
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @Configuration
-public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+public class WebMvcConfiguration implements WebMvcConfigurer {
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
