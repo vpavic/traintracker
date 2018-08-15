c48a79d48816c755a5cfae103df0d432a65cef3a
Vedran Pavic <vedran.pavic@gmail.com>
2018-08-15 22:50:36 +0200

    Improve security headers

--- src/main/java/io/traintracker/config/SecurityConfiguration.java
+++ src/main/java/io/traintracker/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package io.traintracker.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter.ReferrerPolicy;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -20,7 +21,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.httpStrictTransportSecurity()
 					.and()
 				.contentSecurityPolicy("default-src https: 'self'; img-src https: data: 'self'; "
-						+ "script-src https: 'self' 'sha256-GV92UCCdl4ev/6K7wyHn39c/5lPZO6fNFOTT3dDz4L4='");
+						+ "script-src https: 'self' 'sha256-GV92UCCdl4ev/6K7wyHn39c/5lPZO6fNFOTT3dDz4L4='")
+					.and()
+				.referrerPolicy(ReferrerPolicy.NO_REFERRER_WHEN_DOWNGRADE);
 		// @formatter:on
 	}
 
