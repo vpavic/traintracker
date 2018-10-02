5734911515fa6d70a602baa34927716e34bbcef7
Vedran Pavic <vedran.pavic@gmail.com>
2018-10-02 17:57:54 +0200

    Use Redis container in tests

--- src/test/java/io/traintracker/TrainTrackerApplicationTests.java
+++ src/test/java/io/traintracker/TrainTrackerApplicationTests.java
@@ -3,7 +3,12 @@ package io.traintracker;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.GenericContainer;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +16,25 @@ public class TrainTrackerApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@TestConfiguration
+	static class Config {
+
+		@Bean
+		public GenericContainer redisContainer() {
+			GenericContainer redisContainer = new GenericContainer("redis:4.0.11").withExposedPorts(6379);
+			redisContainer.start();
+			return redisContainer;
+		}
+
+		@Bean
+		public LettuceConnectionFactory redisConnectionFactory() {
+			RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(
+					redisContainer().getContainerIpAddress(), redisContainer().getFirstMappedPort());
+			return new LettuceConnectionFactory(configuration);
+		}
+
 	}
 
 }
