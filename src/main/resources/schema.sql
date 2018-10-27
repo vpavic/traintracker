a39af17e7aa74dcf8b0d27a9b1622b3a9f5e5bb6
Vedran Pavic <vedran.pavic@gmail.com>
2018-10-27 10:39:36 +0200

    Improve schema script

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS client_registration (
-  id            VARCHAR(20),
-  client_id     VARCHAR(100),
-  client_secret VARCHAR(100)
+  id            VARCHAR(20) PRIMARY KEY,
+  client_id     VARCHAR(100) NOT NULL UNIQUE,
+  client_secret VARCHAR(100) NOT NULL
 );
