025d5c6867b58e62e1e1f554aca7e6558ef7712f
Vedran Pavic <vedran.pavic@gmail.com>
2018-12-19 22:23:42 +0100

    Revert replace Redis with Hazelcast

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,16 +32,16 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("org.springframework.boot:spring-boot-starter-cloud-connectors")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
-    implementation("org.springframework.session:spring-session-hazelcast")
+    implementation("org.springframework.session:spring-session-data-redis")
 
     implementation("de.vandermeer:asciitable:0.3.2")
     implementation("org.apache.httpcomponents:httpclient")
-    implementation("javax.cache:cache-api")
     implementation("org.jsoup:jsoup:1.11.3")
     implementation("org.postgresql:postgresql")
 
@@ -49,6 +49,7 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.testcontainers:postgresql")
+    testImplementation("org.testcontainers:testcontainers")
 }
 
 checkstyle {
--- src/test/java/io/traintracker/TrainTrackerApplicationTests.java
+++ src/test/java/io/traintracker/TrainTrackerApplicationTests.java
@@ -22,7 +22,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 @RunWith(SpringRunner.class)
@@ -50,6 +53,20 @@ public class TrainTrackerApplicationTests {
             dataSource.setUsername(postgreSqlContainer().getUsername());
             dataSource.setPassword(postgreSqlContainer().getPassword());
             return dataSource;
+        }
+
+        @Bean
+        public GenericContainer redisContainer() {
+            GenericContainer redisContainer = new GenericContainer("redis:4.0.11").withExposedPorts(6379);
+            redisContainer.start();
+            return redisContainer;
+        }
+
+        @Bean
+        public LettuceConnectionFactory redisConnectionFactory() {
+            RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(
+                    redisContainer().getContainerIpAddress(), redisContainer().getFirstMappedPort());
+            return new LettuceConnectionFactory(configuration);
         }
 
     }
