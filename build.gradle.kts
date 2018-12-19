ce6b28ff0672f5936295d0eec1ef407efe9d31a1
Vedran Pavic <vedran.pavic@gmail.com>
2018-12-19 22:05:36 +0100

    Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ configurations {
 
 dependencies {
     implementation(platform("org.springframework.boot:spring-boot-dependencies:2.1.1.RELEASE"))
-    implementation(platform("org.testcontainers:testcontainers-bom:1.10.2"))
+    implementation(platform("org.testcontainers:testcontainers-bom:1.10.3"))
 
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-cache")
@@ -52,5 +52,5 @@ dependencies {
 }
 
 checkstyle {
-    toolVersion = "8.14"
+    toolVersion = "8.15"
 }
