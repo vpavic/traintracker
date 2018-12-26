b9556244d6aab15f2be4ee67397804a0526fe0a4
Vedran Pavic <vedran.pavic@gmail.com>
2018-12-26 12:39:16 +0100

    Remove Redis dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,14 +32,14 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("org.springframework.boot:spring-boot-starter-cloud-connectors")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
-    implementation("org.springframework.session:spring-session-data-redis")
+    implementation("org.springframework.session:spring-session-jdbc")
 
+    implementation("com.github.ben-manes.caffeine:caffeine")
     implementation("de.vandermeer:asciitable:0.3.2")
     implementation("org.apache.httpcomponents:httpclient")
     implementation("org.jsoup:jsoup:1.11.3")
@@ -49,7 +49,6 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.testcontainers:postgresql")
-    testImplementation("org.testcontainers:testcontainers")
 }
 
 checkstyle {
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,35 @@
-CREATE TABLE IF NOT EXISTS client_registration (
+CREATE TABLE IF NOT EXISTS client_registration
+(
   id            VARCHAR(20) PRIMARY KEY,
   client_id     VARCHAR(100) NOT NULL UNIQUE,
   client_secret VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS user_profile (
+CREATE TABLE IF NOT EXISTS user_profile
+(
   email       VARCHAR(50) PRIMARY KEY,
   first_login BIGINT NOT NULL,
   last_login  BIGINT NOT NULL,
   favorites   JSONB  NOT NULL DEFAULT '{}' :: JSONB
+);
+
+CREATE TABLE IF NOT EXISTS spring_session
+(
+  primary_id            CHAR(36) PRIMARY KEY,
+  session_id            CHAR(36) NOT NULL UNIQUE,
+  creation_time         BIGINT   NOT NULL,
+  last_access_time      BIGINT   NOT NULL,
+  max_inactive_interval INT      NOT NULL,
+  expiry_time           BIGINT   NOT NULL,
+  principal_name        VARCHAR(100)
+);
+
+CREATE INDEX ON spring_session (expiry_time);
+CREATE INDEX ON spring_session (principal_name);
+
+CREATE TABLE IF NOT EXISTS spring_session_attributes
+(
+  session_primary_id CHAR(36)     NOT NULL REFERENCES spring_session (primary_id) ON DELETE CASCADE,
+  attribute_name     VARCHAR(200) NOT NULL,
+  attribute_bytes    BYTEA        NOT NULL
 );
