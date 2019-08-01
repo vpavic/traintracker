73a502927216e2b923e3558130edd481681fd96a
Vedran Pavic <vedran.pavic@gmail.com>
2019-08-02 00:04:17 +0200

    Upgrade dependencies

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -1,8 +1,8 @@
 object PluginVersions {
-    val jib = "1.3.0"
+    val jib = "1.4.0"
     val kotlin = "1.3.41"
-    val ktlint = "8.1.0"
-    val spotless = "3.23.1"
+    val ktlint = "8.2.0"
+    val spotless = "3.24.0"
     val springBoot = "2.1.6.RELEASE"
     val versions = "0.21.0"
 }
@@ -10,5 +10,5 @@ object PluginVersions {
 object DependencyVersions {
     val asciiTable = "0.3.2"
     val jsoup = "1.12.1"
-    val testcontainers = "1.11.4"
+    val testcontainers = "1.12.0"
 }
