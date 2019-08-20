0ab306da936aab18eab9ab9b97e7e3eefee9197c
Vedran Pavic <vedran.pavic@gmail.com>
2019-08-20 18:04:19 +0200

    Upgrade Gradle and build plugins

--- buildSrc/src/main/kotlin/versions.kt
+++ buildSrc/src/main/kotlin/versions.kt
@@ -1,8 +1,8 @@
 object PluginVersions {
-    val jib = "1.4.0"
+    val jib = "1.5.0"
     val kotlin = "1.3.41"
     val ktlint = "8.2.0"
-    val spotless = "3.24.0"
+    val spotless = "3.24.2"
     val springBoot = "2.2.0.M5"
     val versions = "0.22.0"
 }
