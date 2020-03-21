06b08daaca387b0ef1e758e9c4c07c1b8f049979
Vedran Pavic <vedran.pavic@gmail.com>
2020-03-22 00:51:37 +0100

    Simplify security configuration

--- src/main/java/io/traintracker/config/SecurityConfiguration.java
+++ src/main/java/io/traintracker/config/SecurityConfiguration.java
@@ -16,54 +16,31 @@
 
 package io.traintracker.config;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter.ReferrerPolicy;
 
 @Configuration
-@EnableConfigurationProperties(SecurityProperties.class)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    private final SecurityProperties properties;
-
-    public SecurityConfiguration(SecurityProperties properties) {
-        this.properties = properties;
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // @formatter:off
         http
-            .authorizeRequests()
-                .anyRequest().permitAll()
-                .and()
-            .oauth2Login()
-                .loginPage("/")
-                .and()
-            .headers()
-                .httpStrictTransportSecurity()
-                    .and()
-                .contentSecurityPolicy(
-                    "default-src https: 'self'; img-src https: data: 'self'; " +
-                        "script-src https: 'self' 'sha256-CW5fKI3jV5qwoLYOgIrchxDYRW5DcBigpTBsFB6U8i8='"
-                )
-                    .and()
-                .referrerPolicy(ReferrerPolicy.NO_REFERRER_WHEN_DOWNGRADE)
-                    .and()
-                .featurePolicy("accelerometer 'none'; ambient-light-sensor 'none'; camera 'none'; "
-                        + "encrypted-media 'none'; fullscreen 'none'; geolocation 'none'; "
-                        + "gyroscope 'none'; magnetometer 'none'; microphone 'none'; midi 'none'; "
-                        + "payment 'none'; speaker 'none'; sync-xhr 'none'; usb 'none'; vr 'none'");
+            .authorizeRequests(urlRegistry -> urlRegistry.anyRequest().permitAll())
+            .oauth2Login(configurer -> configurer.loginPage("/"))
+            .headers(configurer -> {
+                configurer.httpStrictTransportSecurity();
+                configurer.contentSecurityPolicy("default-src https: 'self'; img-src https: data: 'self'; "
+                        + "script-src https: 'self' 'sha256-CW5fKI3jV5qwoLYOgIrchxDYRW5DcBigpTBsFB6U8i8='");
+                configurer.referrerPolicy(ReferrerPolicy.NO_REFERRER_WHEN_DOWNGRADE);
+                configurer.featurePolicy("accelerometer 'none'; ambient-light-sensor 'none'; camera 'none'; "
+                        + "encrypted-media 'none'; fullscreen 'none'; geolocation 'none'; gyroscope 'none'; "
+                        + "magnetometer 'none'; microphone 'none'; midi 'none'; payment 'none'; speaker 'none'; "
+                        + "sync-xhr 'none'; usb 'none'; vr 'none'");
+            });
         // @formatter:on
-        if (this.properties.isRequireSecureChannel()) {
-            // @formatter:off
-            http
-                .requiresChannel()
-                    .anyRequest().requiresSecure();
-            // @formatter:on
-        }
     }
 
 }
