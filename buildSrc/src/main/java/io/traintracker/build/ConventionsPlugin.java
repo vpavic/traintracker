6aa9e4424dc3148ee5e1d4cbc084bf6ab79e9f78
Vedran Pavic <vedran.pavic@gmail.com>
2020-03-22 19:11:43 +0100

    Improve build conventions

--- buildSrc/src/main/java/io/traintracker/build/ConventionsPlugin.java
+++ buildSrc/src/main/java/io/traintracker/build/ConventionsPlugin.java
@@ -16,22 +16,42 @@
 
 package io.traintracker.build;
 
+import java.io.File;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+import com.github.benmanes.gradle.versions.VersionsPlugin;
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.tasks.TaskContainer;
 
-import java.io.File;
-
+@SuppressWarnings("unused")
 public class ConventionsPlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project project) {
         project.apply(action -> action.from(new File(project.getRootDir(), "gradle/dependency-versions.gradle")));
+        project.getPluginManager().apply(VersionsPlugin.class);
+        TaskContainer tasks = project.getTasks();
+        tasks.withType(DependencyUpdatesTask.class, dependencyUpdatesTask -> {
+            dependencyUpdatesTask.setGradleReleaseChannel("current");
+            dependencyUpdatesTask.rejectVersionIf(candidate -> isNonStable(candidate.getCandidate().getVersion())
+                    && !isNonStable(candidate.getCurrentVersion()));
+        });
         Configuration developmentOnly = project.getConfigurations().create("developmentOnly");
         if ("development".equals(project.findProperty("profile"))) {
-            project.getConfigurations().getByName("runtimeClasspath", configuration ->
-                    configuration.extendsFrom(developmentOnly));
+            project.getConfigurations().getByName("runtimeClasspath",
+                    configuration -> configuration.extendsFrom(developmentOnly));
         }
+    }
+
+    private static boolean isNonStable(String version) {
+        boolean containsStableKeyword = Stream.of("RELEASE", "FINAL", "GA")
+                .anyMatch(s -> version.toUpperCase().contains(s));
+        boolean isStableVersion = Pattern.compile("^[0-9,.v-]+(-r)?$").matcher(version).matches();
+        return !containsStableKeyword && !isStableVersion;
     }
 
 }
