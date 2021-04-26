dc32ca84d2e97746cf406415fbe95e6384c00bfc
Vedran Pavic <vedran.pavic@gmail.com>
2021-04-26 20:31:29 +0200

    Upgrade Kotlin to 1.5

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
         classpath("com.diffplug.spotless:spotless-plugin-gradle:+")
         classpath("com.github.ben-manes:gradle-versions-plugin:+")
         classpath("gradle.plugin.com.google.cloud.tools:jib-gradle-plugin:+")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.+")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.+")
     }
 }
 
