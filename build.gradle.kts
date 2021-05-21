ef7e6bcbaf953b3d3e9ae7f9b7a310b324e4b102
Vedran Pavic <vedran.pavic@gmail.com>
2021-05-21 09:27:39 +0200

    Upgrade Spring Boot to 2.5.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,7 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.springframework.boot:spring-boot-dependencies:2.4.6"))
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:${project.properties["kotlinVersion"]}"))
+    implementation(platform("org.springframework.boot:spring-boot-dependencies:2.5.0"))
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.github.ben-manes.caffeine:caffeine")
     implementation("de.vandermeer:asciitable:0.3.2")
