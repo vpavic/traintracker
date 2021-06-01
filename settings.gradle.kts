d2597e75935b58796155366e3c46fc5bb104e8b1
Vedran Pavic <vedran.pavic@gmail.com>
2021-06-01 22:27:25 +0200

    Upgrade build plugins
    - Gradle versions plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ pluginManagement {
     plugins {
         val kotlinVersion: String by settings
         id("com.diffplug.spotless").version("5.12.5")
-        id("com.github.ben-manes.versions").version("0.38.0")
+        id("com.github.ben-manes.versions").version("0.39.0")
         id("com.google.cloud.tools.jib").version("3.0.0")
         kotlin("jvm").version(kotlinVersion)
         kotlin("plugin.spring").version(kotlinVersion)
