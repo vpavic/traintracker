bf9a6c09e19476426eb66da7044d785c6d6c657b
Vedran Pavic <vedran.pavic@gmail.com>
2021-06-29 21:46:14 +0200

    Upgrade build plugins
    - Spotless

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
     plugins {
         val kotlinVersion: String by settings
-        id("com.diffplug.spotless").version("5.12.5")
+        id("com.diffplug.spotless").version("5.14.0")
         id("com.github.ben-manes.versions").version("0.39.0")
         id("com.google.cloud.tools.jib").version("3.1.1")
         kotlin("jvm").version(kotlinVersion)
