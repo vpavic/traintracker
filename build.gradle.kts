765617e1e5947702274c841fdddd048ebdd5dacc
Vedran Pavic <vedran.pavic@gmail.com>
2021-07-13 23:53:47 +0200

    Use Kotlin BOM to align versions of Kotlin dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ repositories {
 
 dependencies {
     implementation(platform("org.springframework.boot:spring-boot-dependencies:2.5.2"))
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:${project.property("kotlinVersion")}"))
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.github.ben-manes.caffeine:caffeine")
     implementation("de.vandermeer:asciitable:0.3.2")
