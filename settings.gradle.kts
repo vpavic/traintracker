9fb0e900b0ebd6d16cbd7517d2dac7c043ad6804
Vedran Pavic <vedran.pavic@gmail.com>
2021-08-12 23:22:54 +0200

    Upgrade build plugins
    - Jib

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ pluginManagement {
         val kotlinVersion: String by settings
         id("com.diffplug.spotless").version("5.14.2")
         id("com.github.ben-manes.versions").version("0.39.0")
-        id("com.google.cloud.tools.jib").version("3.1.2")
+        id("com.google.cloud.tools.jib").version("3.1.3")
         kotlin("jvm").version(kotlinVersion)
         kotlin("plugin.spring").version(kotlinVersion)
     }
