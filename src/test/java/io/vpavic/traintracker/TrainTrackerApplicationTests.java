524cf2c1374ff8533f7050eb1999a350a95733a8
Vedran Pavic <vpavic@duck.com>
2021-12-26 20:31:00 +0100

    Simplify tests

--- src/test/java/io/vpavic/traintracker/TrainTrackerApplicationTests.java
+++ src/test/java/io/vpavic/traintracker/TrainTrackerApplicationTests.java
@@ -1,48 +1,20 @@
 package io.vpavic.traintracker;
 
-import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.session.jdbc.JdbcIndexedSessionRepository;
-import org.springframework.test.context.ActiveProfiles;
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@SpringBootTest
-@ActiveProfiles("test")
+@SpringBootTest(properties = "spring.flyway.enabled=false")
 class TrainTrackerApplicationTests {
 
+    @MockBean
+    @SuppressWarnings("unused")
+    private DataSource dataSource;
+
     @Test
-    void contextLoads(ApplicationContext applicationContext) {
-        assertThat(applicationContext).isNotNull();
-        assertThat(applicationContext.getBeanNamesForType(CaffeineCacheManager.class)).isNotEmpty();
-        assertThat(applicationContext.getBeanNamesForType(JdbcIndexedSessionRepository.class)).isNotEmpty();
-    }
-
-    @TestConfiguration
-    static class Config {
-
-        @Bean
-        PostgreSQLContainer<?> postgreSqlContainer() {
-            PostgreSQLContainer<?> postgreSqlContainer = new PostgreSQLContainer<>("postgres:11.5");
-            postgreSqlContainer.start();
-            return postgreSqlContainer;
-        }
-
-        @Bean
-        HikariDataSource dataSource(PostgreSQLContainer<?> postgreSqlContainer) {
-            HikariDataSource dataSource = new HikariDataSource();
-            dataSource.setJdbcUrl(postgreSqlContainer.getJdbcUrl());
-            dataSource.setUsername(postgreSqlContainer.getUsername());
-            dataSource.setPassword(postgreSqlContainer.getPassword());
-            return dataSource;
-        }
-
+    void contextLoads() {
     }
 
 }
