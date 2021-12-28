0e14c27cbff9a1d83c1c81d243feebfd603194ed
Vedran Pavic <vpavic@duck.com>
2021-12-28 14:42:55 +0100

    Add Heroku environment post processor
    This commit adds Heroku specific implementation of environment post processor that maps JDBC and Redis URLs from env to established configuration properties.

---
+++ src/main/java/io/vpavic/traintracker/infrastructure/cloud/heroku/HerokuEnvironmentPostProcessor.java
@@ -0,0 +1,60 @@
+package io.vpavic.traintracker.infrastructure.cloud.heroku;
+
+import java.util.Properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.cloud.CloudPlatform;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.core.env.CommandLinePropertySource;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.util.StringUtils;
+
+/**
+ * An {@link EnvironmentPostProcessor} that maps Heroku env variables to appropriate Spring Boot configuration
+ * properties.
+ * <p>
+ * Supported env variables and their respective mapped properties are:
+ * <ul>
+ * <li>{@code DATABASE_URL} - {@code spring.datasource.url}</li>
+ * <li>{@code REDIS_TLS_URL} - {@code spring.redis.url}</li>
+ * </ul>
+ */
+public class HerokuEnvironmentPostProcessor implements EnvironmentPostProcessor {
+
+    @Override
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        if (CloudPlatform.HEROKU.isActive(environment)) {
+            Properties properties = new Properties();
+            extractDatabaseUrl(environment, properties);
+            extractRedisUrl(environment, properties);
+            PropertiesPropertySource herokuPropertySource = new PropertiesPropertySource("heroku", properties);
+            MutablePropertySources propertySources = environment.getPropertySources();
+            if (propertySources.contains(CommandLinePropertySource.COMMAND_LINE_PROPERTY_SOURCE_NAME)) {
+                propertySources.addAfter(CommandLinePropertySource.COMMAND_LINE_PROPERTY_SOURCE_NAME,
+                        herokuPropertySource);
+            }
+            else {
+                propertySources.addFirst(herokuPropertySource);
+            }
+        }
+    }
+
+    private static void extractDatabaseUrl(ConfigurableEnvironment environment, Properties properties) {
+        String databaseUrl = environment.getProperty("DATABASE_URL");
+        if (StringUtils.hasText(databaseUrl)) {
+            if (databaseUrl.startsWith("postgres:")) {
+                properties.setProperty("spring.datasource.url", databaseUrl.replace("postgres:", "jdbc:postgresql:"));
+            }
+        }
+    }
+
+    private static void extractRedisUrl(ConfigurableEnvironment environment, Properties properties) {
+        String redisUrl = environment.getProperty("REDIS_TLS_URL");
+        if (StringUtils.hasText(redisUrl)) {
+            properties.setProperty("spring.redis.url", redisUrl);
+        }
+    }
+
+}
---
+++ src/test/java/io/vpavic/traintracker/infrastructure/cloud/heroku/HerokuEnvironmentPostProcessorTests.java
@@ -0,0 +1,46 @@
+package io.vpavic.traintracker.infrastructure.cloud.heroku;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.support.TestPropertySourceUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link HerokuEnvironmentPostProcessor}.
+ */
+class HerokuEnvironmentPostProcessorTests {
+
+    private final HerokuEnvironmentPostProcessor environmentPostProcessor = new HerokuEnvironmentPostProcessor();
+
+    private final ConfigurableApplicationContext context = new AnnotationConfigApplicationContext();
+
+    @BeforeEach
+    void setUp() {
+        TestPropertySourceUtils.addInlinedPropertiesToEnvironment(this.context, "DYNO=test");
+    }
+
+    @Test
+    void postProcessEnvironment_WithDatabaseUrl_ShouldSetSpringDatasourceUrl() {
+        TestPropertySourceUtils.addInlinedPropertiesToEnvironment(this.context,
+                "DATABASE_URL=postgres://username:password@example.com:5432/test");
+        this.environmentPostProcessor.postProcessEnvironment(this.context.getEnvironment(), null);
+        assertThat(getProperty("spring.datasource.url"))
+                .isEqualTo("jdbc:postgresql://username:password@example.com:5432/test");
+    }
+
+    @Test
+    void postProcessEnvironment_WithRedisUrl_ShouldSetSpringRedisUrl() {
+        TestPropertySourceUtils.addInlinedPropertiesToEnvironment(this.context,
+                "REDIS_TLS_URL=rediss://:password@example.com:6379");
+        this.environmentPostProcessor.postProcessEnvironment(this.context.getEnvironment(), null);
+        assertThat(getProperty("spring.redis.url")).isEqualTo("rediss://:password@example.com:6379");
+    }
+
+    private String getProperty(String key) {
+        return this.context.getEnvironment().getProperty(key);
+    }
+
+}
