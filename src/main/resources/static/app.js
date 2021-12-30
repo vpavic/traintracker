e7ed666d3f9161c164524c319e14fec7b1eb11f5
Vedran Pavic <vpavic@duck.com>
2021-12-30 21:16:39 +0100

    Reformat HTML and JavaScript sources

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -3,43 +3,43 @@ var input = $('#train-no-input');
 var button = $('#submit-button');
 
 Mousetrap.bind('/', function () {
-    input.focus();
+	input.focus();
 });
 Mousetrap.bind('h', function () {
-    $('#stations').collapse('toggle');
+	$('#stations').collapse('toggle');
 });
 Mousetrap.bind('r', function () {
-    form.submit();
+	form.submit();
 });
 Mousetrap.bind('c', function () {
-    $('#carrier').modal('show');
+	$('#carrier').modal('show');
 });
 Mousetrap.bind('?', function () {
-    $('#shortcuts').modal('show');
+	$('#shortcuts').modal('show');
 });
 Mousetrap.bindGlobal('esc', function () {
-    input.blur();
+	input.blur();
 });
 
 $(document).ready(function () {
-    if (input.val()) {
-        input.keyup();
-    }
+	if (input.val()) {
+		input.keyup();
+	}
 });
 
 input.keyup(function () {
-    var value = $(this).val();
-    var empty = value.length == 0;
-    var disabled = button.is(':disabled');
+	var value = $(this).val();
+	var empty = value.length == 0;
+	var disabled = button.is(':disabled');
 
-    if (!empty) {
-        form.attr('action', value);
-        if (disabled) {
-            button.removeAttr('disabled');
-        }
-    }
-    else if (!disabled) {
-        form.removeAttr('action');
-        button.attr('disabled', 'disabled');
-    }
+	if (!empty) {
+		form.attr('action', value);
+		if (disabled) {
+			button.removeAttr('disabled');
+		}
+	}
+	else if (!disabled) {
+		form.removeAttr('action');
+		button.attr('disabled', 'disabled');
+	}
 });
