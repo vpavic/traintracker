b638d1df4fea64adcf69750270d658d688e307b7
Vedran Pavic <vpavic@duck.com>
2022-01-03 23:03:01 +0100

    Update frontend dependencies
    - Bootstrap 5.1
    - drop jQuery
    - drop Font Awesome

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,45 +1,38 @@
-var form = $('#search-form');
-var input = $('#train-no-input');
-var button = $('#submit-button');
+let trainNoInput = document.getElementById("train-no-input");
+let searchForm = document.getElementById("search-form");
+let carrierModal = new bootstrap.Modal(document.getElementById("carrier"));
+let shortcutsModal = new bootstrap.Modal(document.getElementById("shortcuts"));
+let submitButton = document.getElementById("submit-button");
 
-Mousetrap.bind('/', function () {
-	input.focus();
+Mousetrap.bind("/", function () {
+	trainNoInput.focus();
 });
-Mousetrap.bind('h', function () {
-	$('#stations').collapse('toggle');
+Mousetrap.bind("r", function () {
+	searchForm.submit();
 });
-Mousetrap.bind('r', function () {
-	form.submit();
+Mousetrap.bind("c", function () {
+	carrierModal.show();
 });
-Mousetrap.bind('c', function () {
-	$('#carrier').modal('show');
+Mousetrap.bind("?", function () {
+	shortcutsModal.show();
 });
-Mousetrap.bind('?', function () {
-	$('#shortcuts').modal('show');
-});
-Mousetrap.bindGlobal('esc', function () {
-	input.blur();
+Mousetrap.bindGlobal("esc", function () {
+	trainNoInput.blur();
 });
 
-$(document).ready(function () {
-	if (input.val()) {
-		input.keyup();
+document.onreadystatechange = function () {
+	if (trainNoInput.value) {
+		trainNoInput.select();
 	}
-});
+};
 
-input.keyup(function () {
-	var value = $(this).val();
-	var empty = value.length == 0;
-	var disabled = button.is(':disabled');
-
-	if (!empty) {
-		form.attr('action', value);
-		if (disabled) {
-			button.removeAttr('disabled');
+trainNoInput.onkeyup = function () {
+	if (this.value.length !== 0) {
+		if (submitButton.disabled) {
+			submitButton.removeAttribute("disabled");
 		}
 	}
-	else if (!disabled) {
-		form.removeAttr('action');
-		button.attr('disabled', 'disabled');
+	else if (!submitButton.disabled) {
+		submitButton.setAttribute("disabled", "disabled");
 	}
-});
+};
