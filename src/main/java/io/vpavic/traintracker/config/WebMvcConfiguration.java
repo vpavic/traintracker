e0752662a1b35310eda669f28444797dcab217c9
Vedran Pavic <vpavic@duck.com>
2022-01-04 23:41:35 +0100

    Remove RequestDumperFilter

--- src/main/java/io/vpavic/traintracker/config/WebMvcConfiguration.java
+++ src/main/java/io/vpavic/traintracker/config/WebMvcConfiguration.java
@@ -1,10 +1,7 @@
 package io.vpavic.traintracker.config;
 
-import org.apache.catalina.filters.RequestDumperFilter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
@@ -23,14 +20,6 @@ class WebMvcConfiguration implements WebMvcConfigurer {
 		CookieLocaleResolver localeResolver = new CookieLocaleResolver();
 		localeResolver.setCookieName("LOCALE");
 		return localeResolver;
-	}
-
-	@Bean
-	FilterRegistrationBean<RequestDumperFilter> requestDumperFilter() {
-		FilterRegistrationBean<RequestDumperFilter> requestDumperFilter = new FilterRegistrationBean<>(
-				new RequestDumperFilter());
-		requestDumperFilter.setOrder(Ordered.HIGHEST_PRECEDENCE);
-		return requestDumperFilter;
 	}
 
 }
