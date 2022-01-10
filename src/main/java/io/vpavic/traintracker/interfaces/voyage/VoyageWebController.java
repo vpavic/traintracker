b4e5a6fde52e2c4bb70cca36173d9fefc0b4b3a8
Vedran Pavic <vpavic@duck.com>
2022-01-10 20:02:01 +0100

    Simplify voyage search interface
    This commit removes `VoyageFetcherConverter` in favor of `VoyageFetcher` resolution directly in `VoyageWebController`.
    
    Additionally, as a preparation to migration of passing carrier id using voyage search form, the form handler now takes both carrier id and train number using query parameters.

--- src/main/java/io/vpavic/traintracker/interfaces/voyage/VoyageWebController.java
+++ src/main/java/io/vpavic/traintracker/interfaces/voyage/VoyageWebController.java
@@ -1,5 +1,9 @@
 package io.vpavic.traintracker.interfaces.voyage;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpStatus;
@@ -13,26 +17,35 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
 import io.vpavic.traintracker.application.VoyageFetcher;
+import io.vpavic.traintracker.domain.model.carrier.CarrierId;
 import io.vpavic.traintracker.domain.model.voyage.Station;
 import io.vpavic.traintracker.domain.model.voyage.Voyage;
 
 @Controller
-@RequestMapping(path = "/{carrierId:[a-z]+}", produces = MediaType.TEXT_HTML_VALUE)
+@RequestMapping(path = "/", produces = MediaType.TEXT_HTML_VALUE)
 class VoyageWebController {
 
+	private final Map<CarrierId, VoyageFetcher> fetchers;
+
+	VoyageWebController(List<VoyageFetcher> fetchers) {
+		this.fetchers = fetchers.stream()
+				.collect(Collectors.toUnmodifiableMap(fetcher -> fetcher.getCarrier().getId(), fetcher -> fetcher));
+	}
+
 	@GetMapping(path = "/voyages", headers = "HX-Request=true")
-	String getVoyageFragment(@PathVariable("carrierId") VoyageFetcher fetcher, @RequestParam("train-no") String train,
-			Model model, HttpServletResponse response) {
-		response.setHeader("HX-Push", "/" + fetcher.getCarrier().getId() + "/" + train);
-		return getVoyage(fetcher, train, model, true);
+	String getVoyageFragment(@RequestParam("carrier-id") CarrierId carrierId, @RequestParam("train-no") String train,
+		Model model, HttpServletResponse response) {
+		response.setHeader("HX-Push", "/" + carrierId + "/" + train);
+		return getVoyage(carrierId, train, model, true);
 	}
 
-	@GetMapping(path = "/{train}")
-	String getVoyagePage(@PathVariable("carrierId") VoyageFetcher fetcher, @PathVariable String train, Model model) {
-		return getVoyage(fetcher, train, model, false);
+	@GetMapping(path = "/{carrierId:[a-z]+}/{train}")
+	String getVoyagePage(@PathVariable("carrierId") CarrierId carrierId, @PathVariable String train, Model model) {
+		return getVoyage(carrierId, train, model, false);
 	}
 
-	private static String getVoyage(VoyageFetcher fetcher, String train, Model model, boolean fragment) {
+	private String getVoyage(CarrierId carrierId, String train, Model model, boolean fragment) {
+		VoyageFetcher fetcher = this.fetchers.get(carrierId);
 		if (fetcher == null) {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 		}
--- src/test/java/io/vpavic/traintracker/interfaces/voyage/VoyageWebControllerTests.java
+++ src/test/java/io/vpavic/traintracker/interfaces/voyage/VoyageWebControllerTests.java
@@ -8,18 +8,22 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import io.vpavic.traintracker.application.VoyageFetcher;
+import io.vpavic.traintracker.domain.model.carrier.Carrier;
 import io.vpavic.traintracker.domain.model.carrier.Carriers;
 import io.vpavic.traintracker.domain.model.voyage.Station;
 import io.vpavic.traintracker.domain.model.voyage.Voyage;
 
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.never;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
@@ -31,49 +35,52 @@ class VoyageWebControllerTests {
 	@Autowired
 	private MockMvc mvc;
 
-	@Autowired
+	@SpyBean
 	private VoyageFetcher voyageFetcher;
 
 	@Test
-	void getVoyageFragment_UnknownCarrierId_ShouldReturnBadRequest() throws Exception {
+	void getVoyageFragment_UnknownCarrierId_ShouldReturnNotFound() throws Exception {
 		// when
-		ResultActions result = this.mvc.perform(get("/test/voyages?train-no=123"));
+		ResultActions result = this.mvc.perform(get("/voyages")
+				.param("carrier-id", "test")
+				.param("train-no", "123"));
 		// then
-		result.andExpect(status().isBadRequest());
+		result.andExpect(status().isNotFound());
+		then(this.voyageFetcher).should(never()).getVoyage(anyString());
 	}
 
 	@Test
 	void getVoyageFragment_ValidCarrierId_ShouldReturnOk() throws Exception {
-		// given
-		given(this.voyageFetcher.getVoyage("123")).willReturn(new Voyage(Carriers.hzpp.getId(), LocalDate.EPOCH,
-				new Station("Test"), List.of(), List.of(), LocalTime.NOON));
 		// when
-		ResultActions result = this.mvc.perform(get("/hzpp/voyages?train-no=123").header("HX-Request", "true"));
+		ResultActions result = this.mvc.perform(get("/voyages")
+				.header("HX-Request", "true")
+				.param("carrier-id", Carriers.hzpp.getId().toString())
+				.param("train-no", "123"));
 		// then
 		result.andExpect(status().isOk());
 		result.andExpectAll(
 				header().string("HX-Push", "/hzpp/123"),
 				content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+		then(this.voyageFetcher).should().getVoyage(eq("123"));
 	}
 
 	@Test
-	void getVoyagePage_UnknownCarrierId_ShouldReturnBadRequest() throws Exception {
+	void getVoyagePage_UnknownCarrierId_ShouldReturnNotFound() throws Exception {
 		// when
 		ResultActions result = this.mvc.perform(get("/test/123"));
 		// then
-		result.andExpect(status().isBadRequest());
+		result.andExpect(status().isNotFound());
+		then(this.voyageFetcher).should(never()).getVoyage(anyString());
 	}
 
 	@Test
 	void getVoyagePage_ValidCarrierId_ShouldReturnOk() throws Exception {
-		// given
-		given(this.voyageFetcher.getVoyage("123")).willReturn(new Voyage(Carriers.hzpp.getId(), LocalDate.EPOCH,
-				new Station("Test"), List.of(), List.of(), LocalTime.NOON));
 		// when
 		ResultActions result = this.mvc.perform(get("/hzpp/123"));
 		// then
 		result.andExpect(status().isOk());
 		result.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+		then(this.voyageFetcher).should().getVoyage(eq("123"));
 	}
 
 	@TestConfiguration(proxyBeanMethods = false)
@@ -81,9 +88,20 @@ class VoyageWebControllerTests {
 
 		@Bean
 		VoyageFetcher voyageFetcher() {
-			VoyageFetcher voyageFetcher = mock(VoyageFetcher.class);
-			given(voyageFetcher.getCarrier()).willReturn(Carriers.hzpp);
-			return voyageFetcher;
+			return new VoyageFetcher() {
+
+				@Override
+				public Carrier getCarrier() {
+					return Carriers.hzpp;
+				}
+
+				@Override
+				public Voyage getVoyage(String train) {
+					return new Voyage(Carriers.hzpp.getId(), LocalDate.EPOCH, new Station("Test"), List.of(), List.of(),
+							LocalTime.NOON);
+				}
+
+			};
 		}
 
 	}
