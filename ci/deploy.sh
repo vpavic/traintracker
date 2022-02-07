a3c99bacf0cfae1795afb2542c935c90359077cb
Vedran Pavic <vpavic@duck.com>
2022-02-07 22:04:44 +0100

    Improve deployment script

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+# build Docker image and publish it to Heroku registry
 ./gradlew jib -Djib.to.image=registry.heroku.com/traintrckr/web -Djib.to.auth.username=_ -Djib.to.auth.password="$(heroku auth:token)"
-heroku config:set SPRING_FLYWAY_ENABLED=true --app traintrckr && heroku container:release web --app traintrckr
+# enable Flyway migrations
+heroku config:set SPRING_FLYWAY_ENABLED=true --app traintrckr
+# deploy application
+heroku container:release web --app traintrckr || true
+# wait for application to start up
 sleep 60
+# disable Flyway migrations
 heroku config:unset SPRING_FLYWAY_ENABLED --app traintrckr
