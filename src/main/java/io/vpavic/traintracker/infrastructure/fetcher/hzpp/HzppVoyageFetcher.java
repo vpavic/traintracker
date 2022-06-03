55c39088ae4d91c5bd4280e6196cf9cff66bcc36
Vedran Pavic <vpavic@duck.com>
2022-06-03 23:39:10 +0200

    Revert "Replace Java HTTP client with Apache HTTP client"
    This reverts commit 2f436b30fb6a5b1efe97c0a8270bba1726e28559.

--- src/main/java/io/vpavic/traintracker/infrastructure/fetcher/hzpp/HzppVoyageFetcher.java
+++ src/main/java/io/vpavic/traintracker/infrastructure/fetcher/hzpp/HzppVoyageFetcher.java
@@ -1,15 +1,16 @@
 package io.vpavic.traintracker.infrastructure.fetcher.hzpp;
 
-import java.io.Closeable;
 import java.io.IOException;
 import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.Charset;
+import java.time.Duration;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.Executors;
 
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
@@ -22,15 +23,29 @@ import io.vpavic.traintracker.domain.model.voyage.VoyageId;
 import io.vpavic.traintracker.infrastructure.fetcher.VoyageFetcher;
 
 @Component
-class HzppVoyageFetcher implements VoyageFetcher, Closeable {
+class HzppVoyageFetcher implements VoyageFetcher {
 
 	private static final Logger logger = LoggerFactory.getLogger(HzppVoyageFetcher.class);
 
-	private final CloseableHttpClient httpClient;
+	private static final HttpResponse.BodyHandler<String> responseBodyHandler = HttpResponse.BodyHandlers.ofString(
+			Charset.forName("windows-1250"));
 
-	HzppVoyageFetcher(CloseableHttpClient hzppHttpClient) {
-		Objects.requireNonNull(hzppHttpClient, "hzppHttpClient must not be null");
-		this.httpClient = hzppHttpClient;
+	private final HttpClient httpClient;
+
+	HzppVoyageFetcher(HttpClient httpClient) {
+		Objects.requireNonNull(httpClient, "httpClient must not be null");
+		this.httpClient = httpClient;
+	}
+
+	HzppVoyageFetcher() {
+		this(defaultHttpClient());
+	}
+
+	private static HttpClient defaultHttpClient() {
+		return HttpClient.newBuilder()
+				.connectTimeout(Duration.ofSeconds(10L))
+				.executor(Executors.newFixedThreadPool(5))
+				.build();
 	}
 
 	@Override
@@ -50,16 +65,25 @@ class HzppVoyageFetcher implements VoyageFetcher, Closeable {
 	}
 
 	private String executeRequest(URI uri) {
-		HttpGet request = new HttpGet(uri);
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(uri)
+				.build();
 		logger.debug("Executing request: {}", request);
-		try (CloseableHttpResponse response = this.httpClient.execute(request)) {
-			logger.debug("Received response: {}", response);
-			return EntityUtils.toString(response.getEntity(), "windows-1250");
+		HttpResponse<String> response;
+		try {
+			response = this.httpClient.send(request, responseBodyHandler);
 		}
 		catch (IOException ex) {
 			logger.warn("Error while executing request: {}", request, ex);
 			throw new IllegalStateException(ex.getMessage(), ex);
 		}
+		catch (InterruptedException ex) {
+			logger.warn("Error while executing request: {}", request, ex);
+			Thread.currentThread().interrupt();
+			throw new IllegalStateException(ex.getMessage(), ex);
+		}
+		logger.debug("Received response: {}", response);
+		return response.body();
 	}
 
 	private static URI buildCurrentPositionRequestUri(VoyageId voyageId) {
@@ -69,11 +93,6 @@ class HzppVoyageFetcher implements VoyageFetcher, Closeable {
 				"&service=tpvl" +
 				"&screen=2";
 		return URI.create(String.format(currentPositionUriTemplate, voyageId));
-	}
-
-	@Override
-	public void close() throws IOException {
-		this.httpClient.close();
 	}
 
 }
--- src/test/java/io/vpavic/traintracker/infrastructure/fetcher/hzpp/HzppVoyageFetcherTests.java
+++ src/test/java/io/vpavic/traintracker/infrastructure/fetcher/hzpp/HzppVoyageFetcherTests.java
@@ -1,12 +1,11 @@
 package io.vpavic.traintracker.infrastructure.fetcher.hzpp;
 
 import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
 import java.util.Optional;
 
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
@@ -27,10 +26,10 @@ import static org.mockito.BDDMockito.given;
 class HzppVoyageFetcherTests {
 
 	@Mock
-	private CloseableHttpClient httpClient;
+	private HttpClient httpClient;
 
 	@Mock
-	private CloseableHttpResponse httpResponse;
+	private HttpResponse<String> httpResponse;
 
 	private HzppVoyageFetcher voyageFetcher;
 
@@ -48,32 +47,30 @@ class HzppVoyageFetcherTests {
 	}
 
 	@Test
-	@Disabled
 	void getVoyage_VoyageDoesNotExist_ShouldReturnEmpty() throws Exception {
 		// given
-		//given((this.httpResponse.body())).willReturn(HzppSampleResponses.currentPositionNotFound);
-		given(this.httpClient.<String>execute(any())).willReturn(this.httpResponse);
+		given((this.httpResponse.body())).willReturn(HzppSampleResponses.currentPositionNotFound);
+		given(this.httpClient.<String>send(any(), any())).willReturn(this.httpResponse);
 		// when
 		Optional<Voyage> result = this.voyageFetcher.getVoyage(VoyageId.of("123"));
 		// then
 		then(result).as("Voyage").isEmpty();
-		BDDMockito.then(this.httpClient).should().execute(any());
+		BDDMockito.then(this.httpClient).should().send(any(), any());
 		BDDMockito.then(this.httpClient).shouldHaveNoMoreInteractions();
 	}
 
 	@Test
-	@Disabled
 	void getVoyage_VoyageExists_ShouldReturnVoyage() throws Exception {
 		// given
 		VoyageId voyageId = VoyageId.of("544");
-		//given(this.httpResponse.body()).willReturn(HzppSampleResponses.currentPositionVoyageInProgress);
-		given(this.httpClient.<String>execute(any())).willReturn(this.httpResponse);
+		given(this.httpResponse.body()).willReturn(HzppSampleResponses.currentPositionVoyageInProgress);
+		given(this.httpClient.<String>send(any(), any())).willReturn(this.httpResponse);
 		// when
 		Optional<Voyage> result = this.voyageFetcher.getVoyage(voyageId);
 		// then
 		then(result).as("Voyage").hasValueSatisfying(voyage ->
 				then(voyage.getId()).as("Voyage id").isEqualTo(voyageId));
-		BDDMockito.then(this.httpClient).should().execute(any());
+		BDDMockito.then(this.httpClient).should().send(any(), any());
 		BDDMockito.then(this.httpClient).shouldHaveNoMoreInteractions();
 	}
 
@@ -81,12 +78,12 @@ class HzppVoyageFetcherTests {
 	void getVoyage_ConnectionError_ShouldThrowException() throws Exception {
 		// given
 		VoyageId voyageId = VoyageId.of("123");
-		given(this.httpClient.execute(any())).willThrow(new IOException("test"));
+		given(this.httpClient.send(any(), any())).willThrow(new IOException("test"));
 		// when
 		IllegalStateException exception = catchIllegalStateException(() -> this.voyageFetcher.getVoyage(voyageId));
 		// then
 		then(exception.getMessage()).as("Exception message").isEqualTo("test");
-		BDDMockito.then(this.httpClient).should().execute(any());
+		BDDMockito.then(this.httpClient).should().send(any(), any());
 		BDDMockito.then(this.httpClient).shouldHaveNoMoreInteractions();
 	}
 
