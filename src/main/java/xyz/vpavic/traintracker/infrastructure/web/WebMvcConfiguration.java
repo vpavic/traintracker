86ad66732305567ddb9393cd2d2fb5287f3d249d
Vedran Pavic <vpavic@duck.com>
2022-07-12 22:56:47 +0200

    Add experimental native WebJars version resolution
    This commit replaces webjars-locator-core with experimental native WebJars version resolution.

--- src/main/java/xyz/vpavic/traintracker/infrastructure/web/WebMvcConfiguration.java
+++ src/main/java/xyz/vpavic/traintracker/infrastructure/web/WebMvcConfiguration.java
@@ -4,10 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import xyz.vpavic.traintracker.infrastructure.webjars.WebJarsVersionResourceResolver;
 
 @Configuration(proxyBeanMethods = false)
 class WebMvcConfiguration implements WebMvcConfigurer {
@@ -20,6 +23,12 @@ class WebMvcConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new LocaleChangeInterceptor());
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:META-INF/resources/webjars/")
+				.resourceChain(true).addResolver(new WebJarsVersionResourceResolver());
 	}
 
 	@Override
